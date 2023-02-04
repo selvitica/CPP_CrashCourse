e79784842f21b90cb89945e381df0d40c4b0398b
ashwi <ashwi@HYPERLAPSE>
2023-02-04 22:43:35 +0530

    Add project files.

---
+++ ConsoleApplication_CPP/ConsoleApplication_CPP.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication_CPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//2-4-2023
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    system("pause>0");
+    /*  shell redirection
+        Discards the standard output of the "pause" command,
+        making the program execution wait until the user presses any key without displaying any message.
+    */
+}
